2696db3b4f51a0939f98b787c2ca6fa43a75761b
Alexandre <alexandrecarlos2@gmail.com>
2020-09-10 12:25:11 -0300

    CRUD ASP.NET CORE Simples atualizado

---
+++ AspNetCore21/AspNetCore_CrudAdo/AspNetCore_CrudAdo/SQL/dbo.Clientes.sql
@@ -0,0 +1,13 @@
+﻿USE CadastroTeste
+CREATE TABLE [dbo].[Clientes] (
+    [ClienteId]   INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]        NVARCHAR (80) NOT NULL,
+    [Endereco]    NVARCHAR (80) NOT NULL,
+    [Numero]      NVARCHAR (10) NOT NULL,
+    [Complemento] NVARCHAR (50) NOT NULL,
+    [Cidade]      NVARCHAR (50) NOT NULL,
+    [Estado]      NVARCHAR (50) NOT NULL,
+    [Cep]         NVARCHAR (8)  NOT NULL,
+    CONSTRAINT [PK_Clientes] PRIMARY KEY CLUSTERED ([ClienteId] ASC)
+);
+
